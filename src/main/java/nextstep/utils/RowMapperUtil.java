951026254c4d2d069a69fa6ea88393068efcb993
gh1224 <rlarkgus0@naver.com>
2023-02-02 16:38:26 +0900

    refactor: RowMapper 클래스별로 분리

--- src/main/java/nextstep/utils/RowMapperUtil.java
+++ src/main/java/nextstep/utils/RowMapperUtil.java
@@ -8,27 +8,33 @@ import nextstep.theme.Theme;
 import org.springframework.jdbc.core.RowMapper;
 
 public class RowMapperUtil {
+    public static final RowMapper<Theme> themeRowMapper = (resultSet, rowNum) -> new Theme(
+            resultSet.getLong("theme.id"),
+            resultSet.getString("theme.name"),
+            resultSet.getString("theme.desc"),
+            resultSet.getInt("theme.price")
+    );
+
+    public static final RowMapper<Schedule> scheduleRowMapper = (resultSet, rowNum) -> new Schedule(
+            resultSet.getLong("schedule.id"),
+            themeRowMapper.mapRow(resultSet, rowNum),
+            resultSet.getDate("schedule.date").toLocalDate(),
+            resultSet.getTime("schedule.time").toLocalTime()
+    );
+
+    public static final RowMapper<Member> memberRowMapper = (resultSet, rowNum) -> new Member(
+            resultSet.getLong("member.id"),
+            resultSet.getString("member.username"),
+            resultSet.getString("member.password"),
+            resultSet.getString("member.name"),
+            resultSet.getString("member.phone"),
+            resultSet.getString("member.role")
+    );
+
     public static final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> new Reservation(
             resultSet.getLong("reservation.id"),
-            new Schedule(
-                    resultSet.getLong("schedule.id"),
-                    new Theme(
-                            resultSet.getLong("theme.id"),
-                            resultSet.getString("theme.name"),
-                            resultSet.getString("theme.desc"),
-                            resultSet.getInt("theme.price")
-                    ),
-                    resultSet.getDate("schedule.date").toLocalDate(),
-                    resultSet.getTime("schedule.time").toLocalTime()
-            ),
-            new Member(
-                    resultSet.getLong("member.id"),
-                    resultSet.getString("member.username"),
-                    resultSet.getString("member.password"),
-                    resultSet.getString("member.name"),
-                    resultSet.getString("member.phone"),
-                    resultSet.getString("member.role")
-            ),
+            scheduleRowMapper.mapRow(resultSet, rowNum),
+            memberRowMapper.mapRow(resultSet, rowNum),
             resultSet.getTimestamp("reservation.created_datetime").toLocalDateTime()
     );
 
